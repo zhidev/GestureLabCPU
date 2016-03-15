cacb4a18d20d18def932f863759807d754b5b81f
zhidev <Doug@douglass-macbook-air.local>
2016-03-14 22:41:03 -0700

    Milestone2

--- GestureLab/ViewController.swift
+++ GestureLab/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var trayOriginalCenter: CGPoint!
 
+    @IBOutlet var trayView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,27 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        // Absolute (x,y) coordinates in parent view's coordinate system
+        let point = panGestureRecognizer.locationInView(self.view)
+        // Total translation (x,y) over time in parent view's coordinate system
+        let translation = panGestureRecognizer.translationInView(self.view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            trayOriginalCenter = CGPoint(x: trayView.center.x, y: trayView.center.y)
+            print("Gesture began at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
+    
+    
+    
+    
 }
 
